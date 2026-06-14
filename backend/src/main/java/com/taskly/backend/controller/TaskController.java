6b2934cbe42565275e9bd6f563c9219ac9ad9d51
Edric Roland Li <edricrolandli@gmail.com>
2026-06-14 11:17:00 +0700

    Fix errors when connecting frontend to backend

--- backend/src/main/java/com/taskly/backend/controller/TaskController.java
+++ backend/src/main/java/com/taskly/backend/controller/TaskController.java
@@ -6,6 +6,7 @@ import com.taskly.backend.service.TaskService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
+@PreAuthorize("hasRole('USER')")
 public class TaskController {
 
     private final TaskService taskService;
